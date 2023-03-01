672fc086bfc8470e89e6d5cc4129f6a26b243d7a
Koncheny_FNAFer228 <Koncheny_FNAFer228@LAPTOP-BN9SOC5T>
2023-03-01 18:38:01 +0800

    jscvsBVBHDV

--- src/Prikol/MainWindow.java
+++ src/Prikol/MainWindow.java
@@ -2,29 +2,47 @@ package Prikol;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class MainWindow extends JFrame {
+    private JButton but1;
+    private JButton but2;
+    private JButton but3;
+    private JButton but4;
+    private JButton but5;
     public MainWindow(){
         setTitle("Hello World!!!");
 
         //setSize(200, 200);
 
-        JButton but1 = new JButton("Click me");
-        but1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+        but1 = new JButton("Click me");
+        but2 = new JButton("Don't touch me!");
+        but3 = new JButton("NOOOO!!!");
+        but4 = new JButton("Yes!");
+        but5 = new JButton("Yes!");
 
-            }
-        });
-        add(but1);
+        but1.addActionListener(e ->
+                System.out.println("Click!")
+
+        );
+        but2.addActionListener(e -> System.out.println("Why?"));
+        but3.setEnabled(false);
+
+        add(but1, BorderLayout.NORTH);
+        add(but2, BorderLayout.SOUTH);
+        add(but3, BorderLayout.EAST);
+        JPanel centerPanel = new JPanel();
+        centerPanel.add(but4);
+        centerPanel.add(but5);
+        add(centerPanel);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
         setLocationByPlatform(true);
         pack();
 
         setVisible(true);
+
+
 
     }
 }
